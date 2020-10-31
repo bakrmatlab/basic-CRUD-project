fd833990066d69ad2d6c1da07540f7240c29ba8b
Roynul Rohan <43069448+roynulrohan@users.noreply.github.com>
2020-10-31 05:02:04 -0400

    Prep for next step

---
+++ js/App.js
@@ -0,0 +1,16 @@
+// connect element to variable
+let table = document.getElementById('table');
+
+// create element and store in variable
+let item = document.createElement('div');
+
+// set text of element
+item.innerHTML = 'Hello World';
+
+// set css class of item
+item.className = 'item';
+
+// add item element into table element
+table.appendChild(item);
+
+
